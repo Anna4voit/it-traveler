0ff1ea64e6ef73ab91d63d39afdb8ee59c26ade3
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-07-20 23:17:35 +0300

    update-4
    study v-if,v-show (Conditional Rendering), added HomepageVieew.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-import IButton from './components/IButton/IButton.vue'
+import HomepageView from './views/HomepageView.vue'
+
+const isDesktop = window.innerWidth > 1024
+const isTablet = window.innerWidth <= 1024 && innerWidth > 7487
 </script>
 
 <template>
-  <IButton></IButton>
+  <template v-if="isDesktop">
+    <HomepageView />
+  </template>
+  <template v-else-if="isTablet">This is tablet</template>
+  <template v-else>
+    <div v-show="!isDesktop && !isTablet">this is mobile</div>
+  </template>
+  <div v-show="!isDesktop && !isTablet">this is mobile</div>
 </template>
