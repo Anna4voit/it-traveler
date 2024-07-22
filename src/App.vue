d681e89f4259f306105b794de1aaf98275de172a
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-07-22 11:42:04 +0300

    update-7
    added FavoritePlace component, delete/edit button svg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import HomepageView from './views/HomepageView.vue'
+// import HomepageView from './views/HomepageView.vue'
+import FavoritePlace from './components/FavoritePlace/FavoritePlace.vue'
 </script>
 
 <template>
-  <HomepageView />
+  <FavoritePlace />
+  <!-- <HomepageView /> -->
 </template>
