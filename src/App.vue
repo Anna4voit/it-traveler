a79a42587911f787aefe3891badf7b6233b8395a
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-07-25 20:06:41 +0300

    update-8
    worked with List of favorite places

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 // import HomepageView from './views/HomepageView.vue'
-import FavoritePlace from './components/FavoritePlace/FavoritePlace.vue'
+// import FavoritePlace from './components/FavoritePlace/FavoritePlace.vue'
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 </script>
 
 <template>
-  <FavoritePlace />
+  <div class="bg-white h-screen w-[400px]"></div>
+  <FavoritePlaces />
   <!-- <HomepageView /> -->
 </template>
