0ca8732378d49d91eaf1edf1486268b386ab18fe
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-07-27 15:46:10 +0300

    update-10
    study directive {ref}, added counter

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,7 @@ import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 </script>
 
 <template>
-  <div class="bg-white h-screen w-[400px]"></div>
-  <!-- <FavoritePlaces /> -->
+  <div class="bg-white h-screen w-[400px]"><FavoritePlaces /></div>
+
   <!-- <HomepageView /> -->
-  <FavoritePlaces>
-    <div>THis is super slot</div>
-    <template v-slot:label>This is label</template>
-    <template #list>This is list</template>
-  </FavoritePlaces>
 </template>
