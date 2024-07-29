90ec100b6f846d8d7fa3f3b58e4dbd70f2066578
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-07-29 13:20:49 +0300

    update-12
    added props for IButton styles

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,29 +1,17 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
 
 const counter = ref(0)
-const user = ref({ name: 'Tom', age: 14 })
-const superUser = reactive({ name: 'Super user', age: 1000 })
 
 const increment = () => {
   counter.value += 1
-}
-
-const changeUserName = () => {
-  user.value.name = 'Harry Potter'
-}
-
-const changeSuperUserName = () => {
-  superUser.name = 'New super user'
 }
 </script>
 
 <template>
   <div class="px-6">
-    <div class="text-gray">User name: {{ user.name }}</div>
-    <div class="text-gray">Super User name: {{ superUser.name }}</div>
     <div class="text-gray mb-4">Додані маркери ({{ counter }})</div>
     <!-- <FavoritePlace v-for="n in 4" :key="n" /> -->
     <slot name="label"></slot>
@@ -32,8 +20,7 @@ const changeSuperUserName = () => {
     </slot>
 
     <slot></slot>
-    <IButton class="w-full" @click="changeUserName">Change name</IButton>
-    <IButton class="w-full mt-2" @click="changeSuperUserName">Change super name</IButton>
-    <IButton class="w-full mt-10" @click="increment">Додати маркер</IButton>
+
+    <IButton class="w-full mt-10" variant="gradient" @click="increment">Додати маркер</IButton>
   </div>
 </template>
