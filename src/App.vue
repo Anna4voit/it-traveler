39513761ce1e2b4fa14984e7d1aa3c09b3565b7a
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-08-05 22:18:08 +0300

    update-18
    added LoginForm, watch, FormContainer

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
 
 // import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
+import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
 </script>
 
 <template>
   <!-- <div class="bg-white h-screen w-[400px]"><FavoritePlaces /></div> -->
 
   <RegistrationForm @submit="console.log" />
+  <LoginForm @submit="console.log" />
 </template>
