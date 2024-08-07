8f832482b99a0fb3851e1739c189d8e6befa1cee
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-08-07 23:25:54 +0300

    update-20
    added Teleport, button close modal

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,21 @@
 import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
 import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
 import IModal from './components/IModal/IModal.vue'
+import { ref } from 'vue'
+
+const isOpen = ref(true)
+const closeModal = () => {
+  isOpen.value = false
+}
+const openModal = () => {
+  isOpen.value = true
+}
 </script>
 
 <template>
   <!-- <div class="bg-white h-screen w-[400px]"><FavoritePlaces /></div> -->
-
+  <button @click="openModal">Click me</button>
   <RegistrationForm @submit="console.log" />
   <LoginForm @submit="console.log" />
-  <IModal />
+  <IModal v-if="isOpen" @close="closeModal"> Hello modal </IModal>
 </template>
