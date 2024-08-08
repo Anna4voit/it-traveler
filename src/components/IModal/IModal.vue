d0d3f1686fd3032f1d820c48d5c3fd2c1fbcf5cd
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-08-08 16:28:14 +0300

    update-21
    added textarea

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, Teleport } from 'vue'
 import CrossIcon from '../icons/CrossIcon.vue'
 
 const emit = defineEmits(['close'])
@@ -12,7 +12,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <Teleport to="body">
+  <component :is="Teleport" to="body">
     <div
       class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]"
       @click.self="emit('close')"
@@ -24,5 +24,5 @@ onUnmounted(() => {
         <slot></slot>
       </div>
     </div>
-  </Teleport>
+  </component>
 </template>
